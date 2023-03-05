94320a3f41b2d8d69cb867dc99e0146aa0a3d744
Lyubov Martemyanova <martemyanova.liubov@gmail.com>
2023-03-06 02:04:26 +0300

    Add classes Reviewer/Lecturer and def rate_lecture for Student

--- students_info.py
+++ students_info.py
@@ -7,6 +7,18 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecture(self, lecturer, course, grade):
+        if (isinstance(lecturer, Lecturer)
+                and course in lecturer.courses_attached
+                and (course in self.courses_in_progress or course in self.finished_courses)
+                and grade <= 10 and type(grade) == int):
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return "Ошибка!"
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -14,20 +26,32 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+        if (isinstance(student, Student) and course in self.courses_attached
+                and course in student.courses_in_progress):
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
                 student.grades[course] = [grade]
         else:
-            return 'Ошибка'
+            return 'Ошибка!'
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
 cool_mentor.rate_hw(best_student, 'Python', 10)
